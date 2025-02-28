9ce26c41a1d1cf456de12822083eb5af068ac5d0
akarshansonkar <akku.sonker@gmail.com>
2025-02-28 17:34:32 +0530

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => {
 			host: "0.0.0.0",
 		},
 		build: {
-			outDir: "build",
+			outDir: "dist",
 		},
 		plugins: [react(), svgr({ svgrOptions: { icon: true } })],
 	};
